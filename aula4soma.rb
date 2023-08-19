cfd98c8d4d203ea9dabb8f99ebd5f1d7cabddd40
Ronaldo Araujo <ronaj2001@gamil.com>
2023-08-19 15:01:34 -0300

    meu primeiro desafio

---
+++ aula4soma.rb
@@ -0,0 +1,12 @@
+#usuáio digite 1 número
+#pedir so usuário
+print"Digite um número inteiro:  "
+num1 = gets.chomp.to_i
+
+#usário digie outro número
+print"Digite o outro número:  "
+num2 = gets.chomp.to_i
+#some esses números do usuário
+soma = num1 + num2
+#apareça na tela
+puts" O resultado da soma será:  #{soma} "
---
+++ desafio.rb
@@ -0,0 +1,21 @@
+# 1) usuário digita seu nome na tela
+print "Digite seu nome:  "
+
+
+# 2) variável armazenadas
+nome = gets.chomp
+
+print "Digite seu sobrenome: "
+sobrenome = gets.chomp
+
+# 3) armazenando a idade
+print "Digite sua idade:   "
+idade = gets.chomp.to_i
+
+
+# 4) Impressão final do dados da Biblioteca
+puts "OI #{nome}  #{sobrenome}, sua idade é de : #{idade} anos"
+
+
+
+
