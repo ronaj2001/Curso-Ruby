28af239ddd2f9d05ce9fc31a7f0dcd32632eb1e2
Ronaldo Araujo <ronaj2001@gamil.com>
2023-08-20 20:19:36 -0300

    continuando o curso

---
+++ else.rb
@@ -0,0 +1,11 @@
+#analise un dia da semana
+#SE esse dia da semana for domingo
+#IMPRIMA que o nosso almoço será especial
+dia = 'sabado'
+if dia == 'domingo' #== é uma comparação
+    almoco ='especial'
+else
+    almoco = 'normal'
+end
+#imprime --> puts
+puts "hoje nosso almoço será #{almoco}"
---
+++ if.rb
@@ -0,0 +1,9 @@
+#analise un dia da semana
+#SE esse dia da semana for domingo
+#IMPRIMA que o nosso almoço será especial
+dia = 'domingo'
+if dia == 'domingo' #== é uma comparação
+    almoco ='especial'
+end
+#imprime --> puts
+puts "hoje nosso almoço será #{almoco} "
